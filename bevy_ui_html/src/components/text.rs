452da5964ce6612a606e5ca14a018a42baf38c2b
Eric Kwoka <43540491+ekwoka@users.noreply.github.com>
2026-03-09 21:41:28 +0400

    :construction: Creates TextColor handling

--- bevy_ui_html/src/components/text.rs
+++ bevy_ui_html/src/components/text.rs
@@ -111,3 +111,44 @@ impl ToTokens for TextLayout {
         });
     }
 }
+
+#[derive(Clone, Debug)]
+pub struct TextColor {
+    attributes: Vec<Attribute>,
+}
+
+impl TextColor {
+    const KEYS: [&'static str; 1] = ["color"];
+
+    pub fn ok(self) -> Option<Self> {
+        if self.attributes.is_empty() {
+            None
+        } else {
+            Some(self)
+        }
+    }
+}
+
+impl From<&Vec<Attribute>> for TextColor {
+    fn from(attributes: &Vec<Attribute>) -> Self {
+        Self {
+            attributes: attributes
+                .iter()
+                .filter(|attr| Self::KEYS.contains(&attr.key.as_str()))
+                .cloned()
+                .collect(),
+        }
+    }
+}
+
+impl ToTokens for TextColor {
+    fn to_tokens(&self, tokens: &mut TokenStream) {
+        let color = Value::new(&self.attributes[0].value);
+
+        if let Some(color) = color.parse_as_color() {
+            tokens.extend(quote! {
+                ::bevy::ui::TextColor::all(#color)
+            })
+        }
+    }
+}
